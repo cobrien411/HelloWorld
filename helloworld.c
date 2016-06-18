738d7f39b0a9b615d59804c8aa4b97f3b4dd59a7
cobrien411 <cobrien411@yahoo.com>
2016-06-18 22:49:41 +0000

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,16 @@
+//
+//  helloworld.c
+//  
+//
+//  Created by Chris O'Brien on 6/18/16.
+//
+//
+
+#include<stdio.h>
+#include "helloworld.h"
+
+main()
+{
+
+    printf("hello, world!\n");
+}
---
+++ helloworld.h
@@ -0,0 +1,14 @@
+//
+//  helloworld.h
+//  
+//
+//  Created by Chris O'Brien on 6/18/16.
+//
+//
+
+#ifndef helloworld_h
+#define helloworld_h
+
+#include <stdio.h>
+
+#endif /* helloworld_h */
